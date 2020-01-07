1e44b6c5d0af705ec341436573282e5e17b65a65
LubaKaper <liubovkaper@pursuit.com>
2020-01-07 17:06:13 -0500

    added safari link and view animation

--- Unit3-PersonalProject-Fortnite/DetailViewController.swift
+++ Unit3-PersonalProject-Fortnite/DetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class DetailViewController: UIViewController {
     
@@ -25,9 +26,17 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+       // createGradientView()
         
         
     }
+    
+    override func viewDidLayoutSubviews() {
+           super.viewDidLayoutSubviews()
+           
+           createGradientView()
+           
+       }
     
     func updateUI() {
         //updateTime()
@@ -40,9 +49,9 @@ class DetailViewController: UIViewController {
         nameLabel.text = item.name
         nameLabel.textColor = .systemBlue
         priceLabel.text = "Price: \(item.vBucks.description)"
-        priceLabel.textColor = .white
+        priceLabel.textColor = .systemBlue
         rarityLabel.text = "Rarity: \(item.rarity)"
-        rarityLabel.textColor = .white
+        rarityLabel.textColor = .systemBlue
         imageView.getImage(with: item.imageUrl) { (result) in
             switch result {
             case .failure(let appError):
@@ -55,6 +64,78 @@ class DetailViewController: UIViewController {
         }
     }
     
+  
     
+    @IBAction func openURL(_ sender: UIButton) {
+        guard let url = URL(string: "https://fnbr.co/shop") else {
+            return
+        }
+        let safariVC = SFSafariViewController(url: url)
+        present(safariVC, animated: true, completion: nil)
+        safariVC.delegate = self
+    }
+    
+    let gradient = CAGradientLayer()
+    var gradientSet = [[CGColor]]()
+    var currentGradient: Int = 0
+    
+    let colorOne = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+    let colorTwo = #colorLiteral(red: 0.5405164957, green: 0.001436236314, blue: 0.5435551405, alpha: 1).cgColor
+    let colorThree = #colorLiteral(red: 1, green: 0.3984297812, blue: 1, alpha: 1).cgColor
+    let colorFour = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+    
+    func createGradientView() {
+        gradientSet.append([colorOne, colorTwo])
+        gradientSet.append([colorTwo, colorThree])
+        gradientSet.append([colorThree, colorFour])
+        gradientSet.append([colorFour, colorThree])
+        gradientSet.append([colorThree, colorTwo])
+        gradientSet.append([colorTwo, colorOne])
+        
+        gradient.frame = self.view.bounds
+        gradient.colors = gradientSet[currentGradient]
+        gradient.startPoint = CGPoint(x:0, y:1)
+        gradient.endPoint = CGPoint(x:0, y:1)
+        gradient.drawsAsynchronously = true
+        
+        self.view.layer.insertSublayer(gradient, at: 0)
+        animateGradient()
+    
+    }
+    func animateGradient() {
+        // cycle through all the colors, feel free to add more to the set
+        if currentGradient < gradientSet.count - 1 {
+            currentGradient += 1
+        } else {
+            currentGradient = 0
+        }
+        
+        // animate over 3 seconds
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "colors")
+        gradientChangeAnimation.duration = 2.0
+        gradientChangeAnimation.toValue = gradientSet[currentGradient]
+        gradientChangeAnimation.fillMode = CAMediaTimingFillMode.forwards
+        gradientChangeAnimation.isRemovedOnCompletion = false
+        gradientChangeAnimation.delegate = self
+        gradient.add(gradientChangeAnimation, forKey: "gradientChangeAnimation")
+    }
+    
+    
+}
+
+extension DetailViewController: SFSafariViewControllerDelegate {
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension DetailViewController: CAAnimationDelegate {
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if flag {
+            gradient.colors = gradientSet[currentGradient]
+            animateGradient()
+        }
+    }
     
 }
